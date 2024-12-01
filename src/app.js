4b9ab701eafb54779873a9d7a23fc0eeba3b0fe0
Prajwal M <prajwalmallapur123@gmail.com>
2024-12-01 22:02:00 +0530

    Getapi/feed

--- src/app.js
+++ src/app.js
@@ -4,16 +4,24 @@ const User=require("./models/user.js")
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/feed", async(req,res)=>{
+    
+ const data= await User.find({});
+     res.send(data);
+})
+
 app.post("/signup", async(req,res)=>{
-    const user =new User({
-        firstName:"Prajwal",
-        lastName:"Mallapur",
-        emailId:"prajwalmallapur123@gmail.com",
-        age:24 ,
-        gender:"Male"
-    })
-    await user.save();
-    res.status(200).send("Users added successfully");
+    const user =new User(req.body);
+    try{
+        await user.save();
+        res.status(200).send("Users added successfully");
+    }
+    catch(error){
+        res.status(501).send("Not saved ",error);
+    }
+   
 });
 
 // Wait for the database connection before starting the server
