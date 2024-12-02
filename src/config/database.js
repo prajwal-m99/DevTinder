b23e576a519f2eddc7896523fd5342ab0a5f200f
Prajwal M <prajwalmallapur123@gmail.com>
2024-12-02 21:52:54 +0530

    db changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://namastedev:tzamitWl60wLhktw@namastenode.rc211ms.mongodb.net/devTinder"
+    "mongodb+srv://prajwalmallapur123:Prajwal%40123@node.ygps9.mongodb.net/"
   );
 };
 
